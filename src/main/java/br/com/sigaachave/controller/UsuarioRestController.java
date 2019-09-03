77c1e99fb6c79e67e2dfa859be949788232cf855
Daniel <danielmleiros@hotmail.com>
2019-09-03 02:52:40 -0300

    subindo java

---
+++ src/main/java/br/com/sigaachave/controller/UsuarioRestController.java
@@ -0,0 +1,82 @@
+package br.com.sigaachave.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sigaachave.domain.Reserva;
+import br.com.sigaachave.domain.Usuario;
+import br.com.sigaachave.repository.ReservaRepository;
+import br.com.sigaachave.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/sigaachave")
+@CrossOrigin(origins = "http://localhost:3000")
+public class UsuarioRestController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private ReservaRepository reservaRepository;
+	
+	@RequestMapping(value = "/usuarios", method = RequestMethod.GET)
+	public ResponseEntity<List<Usuario>> all() {
+		return new ResponseEntity<List<Usuario>>(usuarioRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/usuarios/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Usuario> get(@PathVariable("id") Long id) {
+		
+		if(usuarioRepository.existsById(id) == false) {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Usuario>(usuarioRepository.getOne(id), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/usuarios/{id}/excluir", method = RequestMethod.DELETE)
+	public ResponseEntity<Usuario> remove(@PathVariable("id") Long id) {
+		
+		if(usuarioRepository.existsById(id) == false) {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		usuarioRepository.deleteById(id);
+		return new ResponseEntity<Usuario>(HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/usuarios/adicionar/{nome}+{senha}+{papel}", method = RequestMethod.POST)
+	public ResponseEntity<Usuario> add(Usuario usuario){
+		
+		usuarioRepository.save(usuario);
+		return new ResponseEntity<>(HttpStatus.OK); 
+	}
+	
+	@RequestMapping(value = "/usuarios/{id}/atualizar/{nome}+{senha}+{papel}", method = RequestMethod.PUT)
+	public ResponseEntity<Usuario> update(@PathVariable("id") Long id, Usuario usuario){
+		
+		if(usuarioRepository.existsById(id) == false) {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		usuarioRepository.save(usuario);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/usuarios/{id}/reservas", method = RequestMethod.GET)
+	public ResponseEntity<List<Reserva>> getByUser(@PathVariable("id") Long id){
+		
+		return new ResponseEntity<List<Reserva>>(reservaRepository.byUserId(id), HttpStatus.OK);
+	}
+}
---
+++ src/main/java/br/com/sigaachave/domain/Reserva.java
@@ -0,0 +1,73 @@
+package br.com.sigaachave.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import br.com.sigaachave.enums.StatusReserva;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Reserva {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String sala;
+	private String data;
+	private boolean isFixo;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusReserva status;
+	
+	@ManyToOne
+	@JoinColumn(name="usuario_id")
+	@JsonIgnore
+	private Usuario usuario;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getSala() {
+		return sala;
+	}
+	public void setSala(String sala) {
+		this.sala = sala;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public StatusReserva getStatus() {
+		return status;
+	}
+	public void setStatus(StatusReserva status) {
+		this.status = status;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public boolean getIsFixo() {
+		return isFixo;
+	}
+	public void setFixo(boolean isFixo) {
+		this.isFixo = isFixo;
+	}
+}
